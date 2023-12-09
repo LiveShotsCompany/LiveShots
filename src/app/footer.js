3f0e296aa5bd447ec93d292543edaa26f5d1cca4
DanielGumbs <daniel.r.gumbs@gmail.com>
2023-12-09 23:45:44 +0100

    Styling, Scrollbar, gesorteerd op competie en Datum toegevoed

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="h-16 bg-green-600 border-t-2 border-green-600"></div>
+      <div className="h-12 bg-green-600 border-t-4 border-white"></div>
     </footer>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -13,52 +13,62 @@ const Home = () => {
       );
       const response = await query.json();
       const matchArray = Object.values(response.data.match);
+      matchArray.sort((a, b) => a.competition_id - b.competition_id);
       SetMatchInfo(matchArray);
     };
     getData();
   }, []);
 
+  const currentDate = new Date().toLocaleDateString();
+  const matchesByLeague = {};
+
+  matchInfo.forEach((match) => {
+    if (!matchesByLeague[match.competition_name]) {
+      matchesByLeague[match.competition_name] = [];
+    }
+    matchesByLeague[match.competition_name].push(match);
+  });
+
+
   console.log(matchInfo);
 
   return (
-    <div className="max-screen-full">
-      <div className="overflow-x-auto scrollbar-thin scrollbar-thumb-orange-500 scrollbar-track-orange-200">
+    <div className="">
+      <div className="">
         <Nav />
-        <div className="bg-gray-200 pt-20 pl-60 pr-60 pb-20">
-          <div className="flex flex-col border border-green-600 rounded-lg items-center justify-center bg-white pt-8 pb-8">
-            <h1 className="mb-4 text-green-600 text-xl font-bold">
-              User Info:
-            </h1>
-            <div className="flex justify-center grid grid-cols-2 gap-8">
-              {matchInfo.map((match) => (
-                <ul
-                  className="flex items-center rounded-lg bg-green-600 text-black border border-green-500 mb-2 p-2"
-                  key={match.away_id}
-                >
-                  <div className="flex flex-row items-center w-full text-white font-bold justify-center space-x-8">
-                    <div className="">
-                      <li className="flex items-center justify-center">
-                        {match.scheduled}
-                      </li>
-                    </div>
-                    <div>
-                      <li className="flex items-center justify-center">
-                        {match.home_name}
-                      </li>
-                      <li className="flex items-center justify-center">
-                        {match.away_name}
-                      </li>
-                    </div>
-                    <div>
-                      <li className="flex items-center justify-center">
-                        {match.score}
-                      </li>
-                    </div>
-                    <li>
-                      <img src="/favorite.svg" alt="favorite" className="h-6" />
-                    </li>
-                  </div>
-                </ul>
+        <div className="flex justify-center bg-gray-200 pt-6 pb-6">
+          <div className="flex flex-col border border-green-600 w-2/5 h-[34rem] rounded-lg items-center justify-center bg-white pt-8 pb-8">
+            <div className="flex justify-left">
+              <h1 className="mb-4 text-green-600 text-2xl font-bold">
+                {currentDate}
+              </h1>
+            </div>
+            <div className="flex justify-center grid grid-cols-1 pl-12 overflow-y-auto scrollbar">
+              {Object.entries(matchesByLeague).map(([leagueId, matches]) => (
+                  <React.Fragment key={leagueId}>
+                    <h2 className="text-xl text-green-600 font-bold mb-4">{matches[0].competition_name}</h2>
+                    {matches.map((match) => (
+                        <ul
+                            className="flex items-center rounded-lg bg-green-600 text-black border border-green-500 mb-2 mr-4 p-2"
+                        >
+                          <div className="flex flex-row items-center w-full text-white font-bold justify-center space-x-8">
+                            <div className="">
+                              <li className="flex items-center justify-center">{match.time}</li>
+                            </div>
+                            <div>
+                              <li className="flex items-center justify-center">{match.home_name}</li>
+                              <li className="flex items-center justify-center">{match.away_name}</li>
+                            </div>
+                            <div>
+                              <li className="flex items-center justify-center">{match.score}</li>
+                            </div>
+                            <li>
+                              <img src="/favorite.svg" alt="favorite" className="h-6" />
+                            </li>
+                          </div>
+                        </ul>
+                    ))}
+                  </React.Fragment>
               ))}
             </div>
           </div>
